6e59f1695ac804e003789b1fc23920e89b392c65
Subrat02 <91091303+Subrat02@users.noreply.github.com>
2022-05-19 23:36:55 +0530

    Delete schedulemanagement directory

--- schedulemanagement/src/main/java/com/hcl/schedulemanagement/service/TrainingTrackerService.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.schedulemanagement.service;
-
-import com.hcl.schedulemanagement.domain.TrainingTracker;
-
-public interface TrainingTrackerService {
-
-	public TrainingTracker saveOrUpdate(TrainingTracker TrainingTracker);
-	
-}
--- schedulemanagement/src/main/java/com/hcl/schedulemanagement/util/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.hcl.schedulemanagement.util;
