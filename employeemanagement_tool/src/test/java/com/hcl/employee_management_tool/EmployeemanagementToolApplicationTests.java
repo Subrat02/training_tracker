033c0c5f9f054f2c0da0a852016fb1dadea79b21
uzmaperween <uzmaperween1999@gmail.com>
2022-05-19 09:58:24 +0530

    initial commit

---
+++ employeemanagement_tool/src/test/java/com/hcl/employee_management_tool/EmployeemanagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.employee_management_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
